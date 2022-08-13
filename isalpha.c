d3c1fb6819ef498f7b413a9061a42986561aca4c
tinitiuset <tinitiuset@gmail.com>
2022-08-13 23:18:08 +0200

    Functions isupper, islower and isalpha

---
+++ isalpha.c
@@ -0,0 +1,20 @@
+int ft_isupper(int c)
+{
+	if (65 < c && c < 90)
+		return (1);
+	return (0);
+}
+
+int ft_islower(int c)
+{
+	if (97 < c && c < 122)
+		return (1);
+	return (0);
+}
+
+int ft_isalpha(int c)
+{
+	if (ft_isupper(c) || ft_islower(c))
+		return (1);
+	return (0);
+}
