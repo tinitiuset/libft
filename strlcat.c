2ec92631b0d328fc9c7afe36fd610f8a2df9df4f
tinitiuset <tinitiuset@gmail.com>
2022-08-30 12:54:57 +0200

    Function strlcat

---
+++ strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlcat.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvalient <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 16:06:12 by mvalient          #+#    #+#             */
+/*   Updated: 2022/08/30 12:51:55 by mvalient         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	char	*d;
+	const char	*s;
+	size_t	n;
+	size_t	dlen;
+
+	d = dst;
+	s = src;
+	n = dstsize;
+	while (n-- && *d)
+		d++;
+	dlen = d - dst;
+	n = dstsize - dlen;
+	if (!n)
+		return (dlen + ft_strlen(s));
+	while (*s)
+	{
+		if (n != 1)
+		{
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+	return (dlen + (s - src));
+}
