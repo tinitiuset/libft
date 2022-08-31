d041d23ed0d822c07a59e698beb33afcc64040db
tinitiuset <tinitiuset@gmail.com>
2022-08-31 14:56:12 +0200

    Norm  adjustments libft header and test

--- libft.h
+++ libft.h
@@ -6,22 +6,24 @@
 /*   By: mvalient <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:42:35 by mvalient          #+#    #+#             */
-/*   Updated: 2022/08/31 10:58:28 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/08/31 14:49:57 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stddef.h>
+# include <stddef.h>
 
 void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(int c);
-int	ft_islower(int c);
-int	ft_isupper(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
+
+int		ft_isalnum(int c);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
