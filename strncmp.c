f58d63c27c4174ea026ff2e2654075f3293d6498
tinitiuset <tinitiuset@gmail.com>
2022-09-02 11:59:20 +0200

    Fixed strncmp

--- strncmp.c
+++ strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:22:50 by mvalient          #+#    #+#             */
-/*   Updated: 2022/08/31 10:50:42 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/02 11:58:30 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!n)
-		return (0);
-	while (!n--)
+	unsigned char	c1;
+	unsigned char	c2;
+
+	while (n-- > 0)
 	{
-		if (*s1 == *s2++)
-			return (*(char *)s1 - *(char *)--s2);
-		if (!*s1++)
-			break ;
+		c1 = (unsigned char) *s1++;
+		c2 = (unsigned char) *s2++;
+		if (c1 != c2)
+			return (c1 - c2);
+		if (!c1)
+			return (0);
 	}
 	return (0);
 }
