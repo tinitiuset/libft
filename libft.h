609f89cdf011efe23ee8ff682f7a6aa381a2990e
tinitiuset <tinitiuset@gmail.com>
2022-09-02 10:31:31 +0200

    libft header reworked

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mvalient <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:42:35 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/01 11:36:25 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/01 16:25:36 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -17,7 +17,6 @@
 # include <string.h>
 
 void	ft_bzero(void *s, size_t n);
-
 int		ft_isalnum(int c);
 int		ft_islower(int c);
 int		ft_isupper(int c);
@@ -25,15 +24,18 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t maxlen);
 size_t	ft_strlen(const char *str);
-
-char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 #endif
