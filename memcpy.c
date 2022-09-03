9c9404a7db14cef1ebd51fee40dfa4fceae23cab
tinitiuset <tinitiuset@gmail.com>
2022-09-03 12:03:09 +0200

    Fixed memcpy to work with null params

--- memcpy.c
+++ memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 13:08:04 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/01 14:28:36 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/03 12:01:55 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char		*d;
 	const char	*s;
 
+	if (!dest && !src)
+		return (dest);
 	d = dest;
 	s = src;
 	while (n--)
