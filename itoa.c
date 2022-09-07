1dd3073425ac02600a86d1bd563348ce9b0c011b
tinitiuset <tinitiuset@gmail.com>
2022-09-07 21:41:41 +0200

    Function itoa

--- itoa.c
+++ itoa.c
@@ -6,34 +6,51 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 15:53:00 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/06 17:17:30 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/07 11:04:54 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_digits(int n)
+{
+	int	i;
+
+	i = 0;
+	if (0 > n || !n)
+		i++;
+	while (n)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
+	char	*s;
+	size_t	digits;
 	size_t	i;
-	int	digits;
-	char	*nb;
-	
-	if (n >= 0)
-	{
-		digits = n;
-		i = 1;
-		while (n > 9)
-		{
-			n = n / 10;
-			i++;
-		}
-		printf("%lu", i);
-		printf("%d", n);
-		nb = malloc(i);
 
-	}
-	else
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	digits = ft_digits(n);
+	s = malloc(digits + 1);
+	if (!s)
+		return (0);
+	s[digits] = '\0';
+	i = 0;
+	if (n < 0)
 	{
-		i = 1;
+		s[0] = '-';
+		n = n * -1;
+		i++;
 	}
+	while (digits-- > i)
+	{
+		s[digits] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (s);
 }
